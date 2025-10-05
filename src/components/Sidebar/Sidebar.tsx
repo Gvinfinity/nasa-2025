636392d4a8e978f71ebd4f78eea9a43882733173
yellowisk <oheitoralmeida2013@gmail.com>
2025-10-05 17:30:20 -0300

    change sharko

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -50,7 +50,7 @@ export const Sidebar = ({ children }: SidebarProps) => {
         <aside className="w-72 h-screen bg-gradient-to-b from-blue-900 to-blue-950 text-white flex flex-col items-start p-4 shadow-2xl relative overflow-hidden overflow-y-auto">
           <div className="flex items-center justify-center gap-3 h-fit">
             <img
-              src="/public/sharko.png"
+              src="/public/sharko_new.png"
               alt="Sharko, the Shark Seer"
               className="w-20 h-20"
             />
